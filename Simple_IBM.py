e9069a5f01bfcb32b21ed6efe89e935ad8d2bda9
Alexnez98 <47017140+Alexnez98@users.noreply.github.com>
2019-02-06 15:32:48 -0800

    Bulit function for reproduction - AN

--- Simple_IBM.py
+++ Simple_IBM.py
@@ -14,9 +14,20 @@ An individual-based model to simulate birth and death among species
 
 # Part 2 (below): define functions
 
- 
-  
-# Part 3 (below): declare objects/variables
+
+# Part 3 
+inds = [0, 1, 2]
+spes = [9, 7, 5] 
+def reproduce(inds, spes):
+  for val in spes:
+    x = choice([0, 1])
+    if x == 1:
+        spes.extend([val])
+    max1 = max(inds) + 1
+    inds.extend([max1])
+    return inds, spes
+
+#(below): declare objects/variables
 
 N = 1000 # Number of individual organisms
 S = 100  # Number of species
