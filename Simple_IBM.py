552554372a31bbd4ac99355cbd2cecbad1e7985e
Alexnez98 <47017140+Alexnez98@users.noreply.github.com>
2019-02-06 17:09:43 -0800

    Built function for death -AN

--- Simple_IBM.py
+++ Simple_IBM.py
@@ -14,34 +14,43 @@ An individual-based model to simulate birth and death among species
 
 # Part 2 (below): define functions
 
-
-# Part 3 
-inds = [0, 1, 2]
-spes = [9, 7, 5] 
 def reproduce(inds, spes):
+  
+  i1 = list(inds)
+  s1 = list(spes)    
+    
   for val in spes:
     x = choice([0, 1])
     if x == 1:
-        spes.extend([val])
-    max1 = max(inds) + 1
-    inds.extend([max1])
-    return inds, spes
+        s1.extend([val])
+        max1 = max(inds) + 1
+        i1.extend([max1])
+  return i1, s1
 
-#(below): declare objects/variables
+
+def death(inds, spes):
+  
+  i1 = list(inds)
+  s1 = list(spes)
+
+  for val in spes:
+    x = choice([0, 1])
+    if x == 1:
+        i1.pop(0)
+        s1.pop(0)
+        print(len(i1),len(s1))
+  return i1, s1
+
+
+# Part 3(below): declare objects/variables
 
 N = 1000 # Number of individual organisms
 S = 100  # Number of species
 
-inds = range(N) # inds is a list from 0 to 999, where values are individual IDs
+inds = list(range(N)) # inds is a list from 0 to 999, where values are individual IDs
 spes = np.random.randint(0, S, N).tolist()
 
+# Part 4 (Below): run model
 
-
-
-
-#print(spes)
-#sys.exit()
-
-
-t = 1000 # Numbers of time steps
-
+inds, spes = reproduce(inds, spes)
+inds, spes = death(inds, spes)
