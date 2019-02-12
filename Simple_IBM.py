ac4548f9861856167b3b8c2708c0336127a47b36
Alexnez98 <47017140+Alexnez98@users.noreply.github.com>
2019-02-11 20:21:17 -0800

    i workking on the comment right now

--- Simple_IBM.py
+++ Simple_IBM.py
@@ -5,7 +5,7 @@ import sys
 import os
 
 # Part 1 (above): Import modules
-mydir = os.path.expanduser('~/GitHub/IBM-Dojo/')
+mydir = os.path.expanduser('~/GitHub/IBM-Dojo/') 
 sys.path.append(mydir)
 
 ###### Model Description ##############
@@ -14,44 +14,44 @@ sys.path.append(mydir)
 An individual-based model to simulate birth and death among species
 '''
 
-
 # Part 2 (below): define functions
 
-def reproduce(inds, spes, x_coords, y_coords):
+def reproduce(inds, spes, x_coords, y_coords): #Made a function an called it reproduce assigned it the list of inds, spes, x_coords, y_coords
   
-  i1 = list(inds)
-  s1 = list(spes)
-  x1 = list(x_coords)
-  y1 = list(y_coords)    
-    
-  for val in spes:
-    x = choice([0, 1])
-    if x == 1:
-        s1.extend([val])
-        max1 = max(inds) + 1
-        i1.extend([max1])
-  return i1, s1, x1, y1
+  i1 = list(inds) # list of inds and asignned it the variable of i1 
+  s1 = list(spes) # list of spes and asignned it the variable of i1
+  x1 = list(x_coords) # list of x_coords and asignned it the variable of i1
+  y1 = list(y_coords) # list of y_coords and asignned it the variable of i1   
+# we assigned the list new variable because we do not want them to point at the same data
 
+  for val in spes:# loop through val of spes                       
+    x = choice([0, 1]) # randomly choice a number between 0 and 1
+    if x == 1: # if x equal one 
+        s1.extend([val]) # list of spes is gonna be extend by val list
+        max1 = max(inds) + 1 # get the max number of inds and assign it to max1
+        i1.extend([max1]) # extend i1 by max1 which has assigned value of max(inds)
+  return i1, s1, x1, y1 # we return i1, s1, x1 and y1  as we dont want the came data from the other list 
+                        # they would both point at the same object
 
-def death(inds, spes, x_coords, y_coords):
+def death(inds, spes, x_coords, y_coords):# made a function called death and assigned the list of inds, spes, x_coords and y_coords
   
-  i1 = list(inds)
-  s1 = list(spes)
-  x1 = list(x_coords)
-  y1 = list(y_coords)
+  i1 = list(inds) # list of inds and assigned it the variable of i1
+  s1 = list(spes) # list of spes and assigned it the variable of s1
+  x1 = list(x_coords) # list of x_coords and assign it the variable of x1
+  y1 = list(y_coords) # list of y_coords and assin it the variable of y1
 
   for val in spes:
-    x = choice([0, 1])
-    if x == 1:
-        i1.pop(0)
-        s1.pop(0)
-  return i1, s1, x1, y1
-
+    x = choice([0, 1]) # randomly choice 1 or 0 
+    if x == 1: # if x is choosen then
+        i1.pop(0) # in list of i1 take out index (0)
+        s1.pop(0) # in list of s1 take out index (0)
+  return i1, s1, x1, y1 # we return i1, s1, x1 and y1  as we dont want the came data from the other list 
+                        # they would both point at the same object
 
 def dispersal(inds, spes, x_coords, y_coords):
-  for num in range(len(spes)):
-    i = randint(0, len(inds))
-    x_coords[i] += uniform(-1, 1)
+  for num in range(len(spes)): # for the range of spes use length for a loop
+    i = randint(0, len(inds)) # randomly choose a number between 0 and the length of inds
+    x_coords[i] += uniform(-1, 1) 
     y_coords[i] += uniform(-1, 1) 
   return inds, spes, x_coords, y_coords
   
@@ -61,13 +61,12 @@ N = 1000 # Number of individual organisms
 S = 100  # Number of species
 
 inds = list(range(N)) # inds is a list from 0 to 999, where values are individual IDs
-spes = np.random.randint(0, S, N).tolist()
-x_coords = [0]*N
-y_coords = [0]*N
-    
+spes = np.random.randint(0, S, N).tolist() # 
+x_coords = [0]*N # have x_coords list be for 0 to the number of individual
+y_coords = [0]*N # have x_coords list be for 0 to the number of individual
 
 # Part 4 (Below): run model
-OUT = open(mydir + 'SimData/inds_data.csv', 'w+')
+OUT = open(mydir + 'SimData/inds_data.csv', 'w+') 
 OUT.close()
 
 OUT = open(mydir + 'SimData/spes_data.csv', 'w+')
@@ -87,7 +86,6 @@ for x in range(10000):
   len_list = [len(inds), len(spes), len(x_coords), len(y_coords)]
   if min(len_list) != max(len_list):
       print(len_list)
-      sys.exit()
       
   # write data to file every 10 time steps
   if x%25 == 0:
