a8f01c4f7299907b6c6bc3f1a9ca742607e56541
Alexnez98 <47017140+Alexnez98@users.noreply.github.com>
2019-10-12 16:54:46 -0600

    Added Pratice folder -AN

---
+++ Pratice_Code/Pratice_Dict.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 16:19:34 2019
+
+@author: AlexanderNez
+"""
+
+iDict = {}
+
+for i in range(100):
+    iDict[i] = {'age' : 19, 'sex' : 1, 'clat' : 0,
+          'clon' : 0, 'healthy' : 1}
+'''
+print()
+print(list(iDict.keys())[0])
+'''
+rkey
