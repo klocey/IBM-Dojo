966c52b8c5b4d7f11f84b1c20e95010517bc2c00
klocey <ken@weecology.org>
2019-11-16 17:12:58 -0700

    edited Practice_Pandas.py -KL

--- Pratice_Code/Pratice_Pandas.py
+++ Pratice_Code/Pratice_Pandas.py
@@ -2,17 +2,27 @@ import pandas as pd
 import sys
 import os
 
-mydir = os.path.expanduser('~/GitHub/IBM-Dojo')
+mydir = os.path.expanduser('~/GitHub/Python-ABMs')
 df = pd.read_csv(mydir + '/Pratice_Code/Pratice_Pandas_Sheets.txt', delimiter = '\t')
 
 ################################### Chapters Houses ############################
-Chapters = df.sort_values('Chapter Name')
+print(df.shape) # 20 rows x 10 columns
+Chapters = df.sort_values('Chapter Name') # inappropriate name
+print(Chapters.shape) # 20 rows x 10 columns
 
-Chapter_inds = len(Chapters['inds'])
-Chapter_males = len(Chapters.loc[(Chapters['Sex'] < 1)])
-Chapter_females = len(Chapters.loc[(Chapters['Sex'] == 1)])
 
-Chapter_sick = Chapters.loc[(Chapters['Healthy']) < 1]
+Chapter_inds = len(Chapters['inds']) # Going to be 20, inappropriate name
+
+Chapter_males = len(Chapters.loc[(Chapters['Sex'] < 1)]) # inappropriate name
+print(Chapter_males)
+Chapter_females = len(Chapters.loc[(Chapters['Sex'] == 1)]) # inappropriate name
+print(Chapter_females)
+
+
+Chapter_sick = Chapters.loc[(Chapters['Healthy']) < 1] # inappropriate name
+print(Chapter_sick.shape)
+
+
 Chapters_sick_males = Chapters.loc[(Chapters['Healthy'] < 1) & (Chapters['Sex'] < 1)]
 Chapters_sick_females = Chapters.loc[(Chapters['Healthy'] < 1) & (Chapters['Sex'] == 1)]
 
@@ -20,29 +30,51 @@ Chapter_age = Chapters['age']
 print(Chapter_age)
 
 #################################### Blue Gap ##################################
-Blue_Gap = df.loc[df['Chapter Name'] == 'Blue Gap']
+BG = df.loc[df['Chapter Name'] == 'Blue Gap']
 
-Blue_Gap_inds = len(Blue_Gap['inds'])
-Blue_Gap_males =len(Blue_Gap.loc[(Blue_Gap['Sex'] < 1)])
-Blue_Gap_females = len(Blue_Gap.loc[(Blue_Gap['Sex'] == 1)])
+BG_inds = len(BG['inds'])
+BG_males =len(BG.loc[(BG['Sex'] < 1)])
+BG_females = len(BG.loc[(BG['Sex'] == 1)])
 
-sick_inds = Blue_Gap.loc[(Blue_Gap['Healthy']) < 1]
-sick_males = Blue_Gap.loc[(Blue_Gap['Healthy'] < 1) & (Blue_Gap['Sex'] < 1)]
-sick_females = Blue_Gap.loc[(Blue_Gap['Healthy'] < 1) & (Blue_Gap['Sex'] == 1)]
+sick_inds = BG.loc[(BG['Healthy']) < 1]
+sick_males = BG.loc[(BG['Healthy'] < 1) & (BG['Sex'] < 1)]
+sick_females = BG.loc[(BG['Healthy'] < 1) & (BG['Sex'] == 1)]
 
-Blue_Gap_age = Blue_Gap['age']
-print(Blue_Gap_age)
-'''
-Blue_Gap_age_0_9 = Blue_Gap.loc[Blue_Gap['age'] > 10]
-Blue_Gap_age_10_19 = (len(Blue_Gap.loc[Blue_Gap['age'] > 20]) - int(len(Blue_Gap_age_0_9)))
-Blue_Gap_age_20_29 = (len(Blue_Gap.loc[Blue_Gap['age'] > 30]) - int(len(Blue_Gap_age_10_19)))
-Blue_Gap_age_30_39 = (len(Blue_Gap.loc[Blue_Gap['age'] > 40]) - int(len(Blue_Gap_age_20_29)))
-Blue_Gap_age_40_49 = (len(Blue_Gap.loc[Blue_Gap['age'] > 50]) - int(len(Blue_Gap_age_30_39)))
-Blue_Gap_age_50_59 = (len(Blue_Gap.loc[Blue_Gap['age'] > 60]) - int(len(Blue_Gap_age_40_49)))
-Blue_Gap_age_60_69 = (len(Blue_Gap.loc[Blue_Gap['age'] > 70]) - int(len(Blue_Gap_age_50_59)))
-Blue_Gap_age_70_79 = (len(Blue_Gap.loc[Blue_Gap['age'] > 80]) - int(len(Blue_Gap_age_60_69)))
-Blue_Gap_age_80 = (len(Blue_Gap.loc[Blue_Gap['age'] > 90]) - int(len(Blue_Gap_age_70_79)))
-'''
+BG_age = BG['age']
+print(BG_age)
+
+
+# Iterate over rows using iterrows
+for i, row in BG.iterrows():
+    if row['age'] < 10:
+        BG_0_9 += 1
+    elif row['age'] < 20:
+        BG_10_19 += 1
+    elif row['age'] < 30:
+        BG_20_29 += 1
+    elif row['age'] < 40:
+        BG_30_39 += 1
+    elif row['age'] < 50:
+        BG_40_49 += 1
+    elif row['age'] < 60:
+        BG_50_59 += 1
+    elif row['age'] < 70:
+        BG_60_69 += 1
+    elif row['age'] < 80:
+        BG_70_79 += 1
+    
+        
+# use shorter names!!
+BG_age_0_9 = BG.loc[BG['age'] > 10]
+BG_age_10_19 = (len(BG.loc[BG['age'] > 20]) - int(len(BG_age_0_9)))
+BG_age_20_29 = (len(BG.loc[BG['age'] > 30]) - int(len(BG_age_10_19)))
+BG_age_30_39 = (len(BG.loc[BG['age'] > 40]) - int(len(BG_age_20_29)))
+BG_age_40_49 = (len(BG.loc[BG['age'] > 50]) - int(len(BG_age_30_39)))
+BG_age_50_59 = (len(BG.loc[BG['age'] > 60]) - int(len(BG_age_40_49)))
+BG_age_60_69 = (len(BG.loc[BG['age'] > 70]) - int(len(BG_age_50_59)))
+BG_age_70_79 = (len(BG.loc[BG['age'] > 80]) - int(len(BG_age_60_69)))
+BG_age_80 = (len(BG.loc[BG['age'] > 90]) - int(len(BG_age_70_79)))
+
 #################################### Chinle ###################################
 Chinle = df.loc[df['Chapter Name'] == 'Chinle']
 
